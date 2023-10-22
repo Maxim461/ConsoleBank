8fff216d4cba4d29df0f93c72d3d7a8f26bacd58
Maxim461 <Max2004lee@mail.ru>
2023-10-22 18:29:09 +0300

    init

--- ConsoleBank/Program.cs
+++ ConsoleBank/Program.cs
@@ -1,161 +1,168 @@
-﻿double BalanceRub = 0;
-double BalanceDollar = 0;
-double BalanceEuro = 0;
-int[,] CurrencyExchangeRate = new int[2, 10];
-bool VarName = false;
-Random rnd = new Random();
+﻿using System;
 
-for (int i = 0; i < CurrencyExchangeRate.GetLength(0); i++)
+class Program
 {
-    for (int j = 0; j < CurrencyExchangeRate.GetLength(1); j++)
+    static double BalanceRub = 0;
+    static double BalanceDollar = 0;
+    static double BalanceEuro = 0;
+    static int[,] CurrencyExchangeRate = new int[3, 10];
+    static Random rnd = new Random();
+
+    static void Main(string[] args)
     {
-        CurrencyExchangeRate[i, j] = rnd.Next(100);
-    }
-}
-
-while (true)
-{
-    Console.Clear();
-    Console.WriteLine("Введите, если вы хотите:");
-    Console.WriteLine("1 - Выполнить отображение баланса во всех валютах;");
-    Console.WriteLine("2 - Выполнить пополнение баланса;");
-    Console.WriteLine("3 - Выполнить отображение курсов валют;");
-    Console.WriteLine("4 - Выполнить перевод из одной валюты в другую.");
-
-    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-
-    switch (keyInfo.Key)
-    {
-        case ConsoleKey.D1:
-            Console.Clear();
-            Console.WriteLine($"На вашем счету {BalanceRub:f2} рублей;");
-            Console.WriteLine($"На вашем счету {BalanceDollar:f2} долларов;");
-            Console.WriteLine($"На вашем счету {BalanceEuro:f2} евро;");
-            Console.WriteLine("Нажмите Enter для выхода.");
-            Console.ReadLine();
-            break;
-
-        case ConsoleKey.D2:
-            Console.Clear();
-            Console.WriteLine("В какой валюте вы хотите пополнить баланс?");
-            Console.WriteLine("Нажмите:");
-            Console.WriteLine("1 для пополнения в Рублях;");
-            Console.WriteLine("2 для пополнения в Долларах;");
-            Console.WriteLine("3 для пополнения в Евро.");
-            Console.WriteLine("Далее нажмите Enter.");
-            int VarUser = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите сумму для пополнения баланса:");
-            if (VarUser == 1)
-            {
-                BalanceRub = int.Parse(Console.ReadLine());
-            }
-            else if (VarUser == 2)
-            {
-                BalanceDollar = int.Parse(Console.ReadLine());
-            }
-            else if (VarUser == 3)
-            {
-                BalanceEuro = int.Parse(Console.ReadLine());
-            }
-            else
-            {
-                Console.WriteLine("Вы не правильно выбрали способ пополнения баланса.");
-            }
-            Console.WriteLine("Нажмите на Enter.");
-            break;
-
-        case ConsoleKey.D3:
-
-            Console.Clear();
-               
-            SortRows(CurrencyExchangeRate);
-            
-            for (int i = 0; i < CurrencyExchangeRate.GetLength(0); i++)
-            {
-                for (int j = 0; j < CurrencyExchangeRate.GetLength(1); j++)
-                {
-                    Console.Write(CurrencyExchangeRate[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-            
-            Console.WriteLine("Нажмите на Enter.");
-            Console.ReadLine();
-            break;
-
-        case ConsoleKey.D4:
-            Console.Clear();
-            Console.WriteLine("Выберите во что вы хотите перевести:");
-            Console.WriteLine("1 - Из рублей в доллары;");
-            Console.WriteLine("2 - Из рубей в евро;");
-            Console.WriteLine("3 - Из долларов в рубли;");
-            Console.WriteLine("4 - Из долларов в евро;");
-            Console.WriteLine("5 - Из евро в рубли;");
-            Console.WriteLine("6 - Из евро в доллары.");
-            Console.WriteLine("Нажмите Enter для выхода.");
-
-            int UserVar = int.Parse(Console.ReadLine());
-            Console.Clear();
-            double minDol = CurrencyExchangeRate[0, 0];
-            double minEuro = CurrencyExchangeRate[0, 0];
-            double minRub = CurrencyExchangeRate[0, 0];
-            if (UserVar == 1)
-            {
-                BalanceDollar = BalanceRub / minDol;
-                BalanceRub = 0;
-            }
-            else if (UserVar == 2)
-            {
-                BalanceEuro = BalanceRub / minEuro;
-                BalanceRub = 0;
-            }
-            else if (UserVar == 3)
-            {
-                BalanceRub = BalanceDollar / minRub;
-                BalanceDollar = 0;
-            }
-            else if (UserVar == 4)
-            {
-                BalanceEuro = BalanceDollar / minEuro;
-                BalanceDollar = 0;
-            }
-            else if (UserVar == 5)
-            {
-                BalanceRub = BalanceEuro / minRub;
-                BalanceEuro = 0;
-            }
-            else if (UserVar == 6)
-            {
-                BalanceDollar = BalanceEuro / minDol;
-                BalanceEuro = 0;
-            }
-            Console.WriteLine("Операция успешно выполнена.");
-            Console.ReadLine();
-            break;
-    }
-}
-    
-static void SortRows(int[,] CurrencyExchangeRate)
-{
-    int Rows = CurrencyExchangeRate.GetLength(0);
-    int Columns = CurrencyExchangeRate.GetLength(1);
-
-    for (int i = 0; i < Rows; i++)
-    {
-        // Создаем временный массив для сортировки строки
-        int[] TempRow = new int[Columns];
-        for (int j = 0; j < Columns; j++)
+        for (int i = 0; i < CurrencyExchangeRate.GetLength(0); i++)
         {
-            TempRow[j] = CurrencyExchangeRate[i, j];
+            for (int j = 0; j < CurrencyExchangeRate.GetLength(1); j++)
+            {
+                CurrencyExchangeRate[i, j] = rnd.Next(100);
+            }
         }
 
-        // Сортируем временный массив
-        Array.Sort(TempRow);
-
-        // Копируем отсортированные значения обратно в исходную строку
-        for (int j = 0; j < Columns; j++)
+        while (true)
         {
-            CurrencyExchangeRate[i, j] = TempRow[j];
+            Console.Clear();
+            Console.WriteLine("Введите, если вы хотите:");
+            Console.WriteLine("1 - Выполнить отображение баланса во всех валютах;");
+            Console.WriteLine("2 - Выполнить пополнение баланса;");
+            Console.WriteLine("3 - Выполнить отображение курсов валют;");
+            Console.WriteLine("4 - Выполнить перевод из одной валюты в другую.");
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.D1:
+                    Console.Clear();
+                    Console.WriteLine($"На вашем счету {BalanceRub:f2} рублей;");
+                    Console.WriteLine($"На вашем счету {BalanceDollar:f2} долларов;");
+                    Console.WriteLine($"На вашем счету {BalanceEuro:f2} евро;");
+                    Console.WriteLine("Нажмите Enter для выхода.");
+                    Console.ReadLine();
+                    break;
+
+                case ConsoleKey.D2:
+                    Console.Clear();
+                    Console.WriteLine("В какой валюте вы хотите пополнить баланс?");
+                    Console.WriteLine("Нажмите:");
+                    Console.WriteLine("1 для пополнения в Рублях;\n2 для пополнения в Долларах;\n3 для пополнения в Евро.");
+                    int VarUser = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите сумму для пополнения баланса:");
+                    double Amount = double.Parse(Console.ReadLine());
+                    if (VarUser == 1)
+                    {
+                        BalanceRub += Amount;
+                    }
+                    else if (VarUser == 2)
+                    {
+                        BalanceDollar += Amount;
+                    }
+                    else if (VarUser == 3)
+                    {
+                        BalanceEuro += Amount;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы не правильно выбрали способ пополнения баланса.");
+                    }
+                    Console.Clear();
+                    Console.WriteLine("Нажмите на Enter.");
+                    Console.ReadLine();
+                    break;
+
+                case ConsoleKey.D3:
+                    Console.Clear();
+                    PrintCurrencyExchangeRates();
+                    Console.WriteLine("Нажмите на Enter.");
+                    Console.ReadLine();
+                    break;
+
+                case ConsoleKey.D4:
+                    Console.Clear();
+
+                    Console.WriteLine("Выберите из какой валюты вы хотите перевести:\n1 - Рубли;\n2 - Доллары;\n3 - Евро:");
+                    int SourceCurrency = int.Parse(Console.ReadLine());
+
+                    Console.Clear();
+                    Console.WriteLine("Выберите в какую валюту вы хотите перевести:\n1 - Рубли;\n2 - Доллары;\n3 - Евро:");
+                    int TargetCurrency = int.Parse(Console.ReadLine());
+
+                    Console.Clear();
+                    Console.WriteLine("Введите сумму перевода:");
+                    double AmountToConvert = double.Parse(Console.ReadLine());
+
+                    double ExchangeRate = CurrencyExchangeRate[SourceCurrency - 1, TargetCurrency - 1];
+                    double ConvertedAmount = ConvertCurrency(AmountToConvert, ExchangeRate);
+
+                    UpdateBalances(SourceCurrency, TargetCurrency, AmountToConvert, ConvertedAmount);
+
+                    Console.WriteLine($"Конвертировано: {AmountToConvert} {GetCurrencyName(SourceCurrency)} -> {ConvertedAmount} {GetCurrencyName(TargetCurrency)}");
+                    Console.WriteLine("Нажмите на Enter.");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+
+    static void PrintCurrencyExchangeRates()
+    {
+        Console.Clear();
+        Console.WriteLine("Курсы валют:");
+        for (int i = 0; i < CurrencyExchangeRate.GetLength(0); i++)
+        {
+            for (int j = 0; j < CurrencyExchangeRate.GetLength(1); j++)
+            {
+                Console.Write(CurrencyExchangeRate[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static double ConvertCurrency(double amount, double exchangeRate)
+    {
+        return amount / exchangeRate;
+    }
+
+    static string GetCurrencyName(int currencyCode)
+    {
+        switch (currencyCode)
+        {
+            case 1:
+                return "рублей";
+            case 2:
+                return "долларов";
+            case 3:
+                return "евро";
+            default:
+                return "неверной валюты";
+        }
+    }
+
+    static void UpdateBalances(int sourceCurrency, int targetCurrency, double sourceAmount, double targetAmount)
+    {
+        if (sourceCurrency == 1)
+        {
+            BalanceRub -= sourceAmount;
+        }
+        else if (sourceCurrency == 2)
+        {
+            BalanceDollar -= sourceAmount;
+        }
+        else if (sourceCurrency == 3)
+        {
+            BalanceEuro -= sourceAmount;
+        }
+
+        if (targetCurrency == 1)
+        {
+            BalanceRub += targetAmount;
+        }
+        else if (targetCurrency == 2)
+        {
+            BalanceDollar += targetAmount;
+        }
+        else if (targetCurrency == 3)
+        {
+            BalanceEuro += targetAmount;
         }
     }
 }
